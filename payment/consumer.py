29e7780cfce1a4c31f3456b930d09fb8eb491735
vivek <bit.viveksh@gmail.com>
2022-12-23 15:27:59 +0100

    add refund event

--- payment/consumer.py
+++ payment/consumer.py
@@ -1,27 +1,26 @@
-from main import redis, Product
+from main import redis, Order
 import time
 
-key = "order_completed"
-group = "inventory-group"
+key = "refund_order"
+group = "payment-group"
 
 try:
     redis.xgroup_create(key, group)
 except:
     print("Group already exists !")
 
+
 while True:
     try:
         # '>' means consume every event in this group with the specified key
         results = redis.xreadgroup(group, key, {key: '>'}, None)
         if results != []:
+            # print(results)
             for result in results:
                 obj = result[1][0][1]
-                try:
-                    product = Product.get(obj['product_id'])
-                    product.quantity = product.quantity - int(obj['quantity'])
-                    product.save()
-                except:
-                    redis.xadd('refund_order', obj, '*')
+                order = Order.get(obj['pk'])
+                order.status = 'refunded'
+                order.save()
 
     except Exception as e:
         print(str(e))
